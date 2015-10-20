77fc319639ef6979a9c22e6eea8366809176f862
ralianz <ra.lianze@gmail.com>
2015-10-20 22:41:31 +0700

    versi 1.0.0

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+$(document).ready(function() {
+  $('.gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+	enabled: true,
+	navigateByImgClick: true,
+	tPrev: 'Previous (Left arrow key)', // title for left button
+	tNext: 'Next (Right arrow key)', // title for right button
+	tCounter: '%curr% of %total%', // markup of counter
+	preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>' + item.el.attr('desc') +'</small>';
+      }
+    }
+
+
+
+
+  });
+});
+
+
+
+
